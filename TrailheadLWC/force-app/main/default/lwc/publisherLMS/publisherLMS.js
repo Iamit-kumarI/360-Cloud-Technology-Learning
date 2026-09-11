f6f39941445578c86a319e29f2d1039310694946
Iamit-kumarI <amitkumaramitkumar15351@gmail.com>
2026-09-11 20:28:05 +0530

    -> update apex to return contact related to accountId, -> publisher updated to show apex contacts in to combobox in combobox selecting contact name gives contact id and now this contact id published to subscriber

--- TrailheadLWC/force-app/main/default/lwc/publisherLMS/publisherLMS.js
+++ TrailheadLWC/force-app/main/default/lwc/publisherLMS/publisherLMS.js
@@ -1,14 +1,38 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+
+import showContact from '@salesforce/apex/PublisherHandlerException.showContact';
 
 export default class PublisherLMS extends LightningElement {
     @wire(MessageContext)
     MessageContext;
 
+    @api recordId;
+    options = [];
+    selectedContact;
+
+    @wire(showContact, { accId: '$recordId' })
+    getContacts({ data, error }) {
+        if (data) {
+            this.options = data.map(contact => ({
+                label: contact.Name,
+                value: contact.Id
+            }));
+        }
+        else if (error) {
+            //add toast
+            console.log('Error in loading data');
+        }
+    }
+
+    handleChange(event) {
+        this.selectedContact = event.detail.value;
+    }
+
     handlePublish(){
         const payload = {
-            name: 'sakshi'
+            name: this.selectedContact//pass here selected contact Id
         };
         publish(this.MessageContext, SAMPLEMC, payload);
     }
