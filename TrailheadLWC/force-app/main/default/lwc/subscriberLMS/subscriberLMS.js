3c799e21960348e0e2f2ace986dd580fc99cb62e
Iamit-kumarI <amitkumaramitkumar15351@gmail.com>
2026-09-15 00:26:16 +0530

    -> update accordian added 3 buttions brank no funcanality for the time being just logging things, accordian B is demo accordain A - A.4 not working in same parent just A.4 handling not its child for its child needs to make seprate functions

--- TrailheadLWC/force-app/main/default/lwc/subscriberLMS/subscriberLMS.js
+++ TrailheadLWC/force-app/main/default/lwc/subscriberLMS/subscriberLMS.js
@@ -1,9 +1,15 @@
 import { LightningElement, wire } from 'lwc';
 import { subscribe, MessageContext, unsubscribe } from 'lightning/messageService';
 import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+import getContactById from '@salesforce/apex/SubscriberHandler.getContactById';
+
 
 export default class SubscriberLMS extends LightningElement {
+    recievedId = '';
+    recievedEmail = '';
     recievedName = '';
+    messagePulbished = false;
+
     messageRecived = false;
     @wire(MessageContext)
     MessageContext;
@@ -20,15 +26,40 @@ export default class SubscriberLMS extends LightningElement {
             this.MessageContext,
             SAMPLEMC,
             (message) => {
-                this.recievedName = message.name;
+                this.recievedId = message.name;
+                this.messagePulbished = !this.messagePulbished;
             }
         );
     }
     handleMessage(message) {
-        this.recievedName = message.name;
+        this.recievedId = message.name;
     }
+
+    @wire(getContactById, { contactId: '$recievedId' })
+    getContacts({ data, error }) {
+        if (data) {
+            this.recievedEmail = data.Email;
+            this.recievedName = data.Name;
+        } else if (error) {
+            this.recievedEmail = 'can not fetch';
+            this.recievedName = 'can not fetch';
+        }
+    }
+
     unsubscribeToMessageChannel() {
         unsubscribe(this.subscription);
         this.subscription = null;
+    }
+    handleSectionToggle(event){
+        console.log(event.detail.openSections);
+    }
+    handleClick3(event){
+        console.log('button 3 have been clicked');
+    }
+    handleClick1(event){
+        console.log('button 1 have been clicked');
+    }
+    handleClick2(event){
+        console.log('button 2 have been clicked');
     }
 }
